f2bc7d4c1f1f7031e01b4866c808269dbd96f978
Jesper Svensson <jesperswe@hotmail.com>
2023-11-09 14:58:35 +0100

    setup shell of user endpoints

---
+++ src/internal/api/user_api.py
@@ -0,0 +1,43 @@
+# Python imports
+
+# Framework imports
+from flask import jsonify
+
+from src.internal import app
+
+
+@app.route('/api/v1/users/create', methods=["POST"])
+def create_user(data):
+    """
+    This API creates a new user
+    :param data:
+    :return:
+    """
+    return jsonify("Created user. NOT IMPLEMENTED")
+
+@app.route('/api/v1/users/get/<id>', methods=["GET"])
+def get_user(id):
+    """
+    This API creates a new user
+    :param id:
+    :return:
+    """
+    return jsonify("Returned user. NOT IMPLEMENTED")
+
+@app.route('/api/v1/users/update', methods=["PUT"])
+def update_user(data):
+    """
+    This API creates a new user
+    :param data:
+    :return:
+    """
+    return jsonify("Updated user. NOT IMPLEMENTED")
+
+@app.route('/api/v1/users/delete/<id>', methods=["DELETE"])
+def update_user(id):
+    """
+    This API creates a new user
+    :param id:
+    :return:
+    """
+    return jsonify("Deleted user. NOT IMPLEMENTED")

4e63abbd189ec67b9673d5d932d6cde6c8a460ae
lindstromemil <emil.lindstrom00@outlook.com>
2023-11-09 14:57:46 +0100

    further work on flask setup

---
+++ src/internal/__init__.py
@@ -0,0 +1,15 @@
+import flask
+from flask_mongoengine import MongoEngine
+
+db = MongoEngine()
+app = flask.Flask("m7011e_app")
+app.config["MONGODB_SETTINGS"] = [
+    {
+        "db": "m7011e",
+        "host": "localhost",
+        "port": 27017,
+        "alias": "default",
+    }
+]
+
+db.init_app(app)

6e03c7f4496c92e5db79a39dbd08e2c628584cb1
filber-9 <filber-9@student.ltu.se>
2023-11-29 15:40:44 +0100

    Add handler for user and brand

--- src/internal/api/brand_api.py
+++ src/internal/api/brand_api.py
@@ -1,51 +1,81 @@
-# Python imports
-
-# Framework imports
-from flask import jsonify
-
+from flask import jsonify, request
+from internal.models.brand import Brand
 from src.internal import app
 
 
 @app.route("/api/v1/brand/create", methods=["POST"])
-def create_brand(data):
+def create_brand():
     """
-    This API creates a new user
-    :param data:
+    This API creates a new brand
+    :param jason data:
     :return:
     """
-    return jsonify("Created brand. NOT IMPLEMENTED")
+    data = request.get_json()
+    if not is_name_unique(data["name"]):
+        raise jsonify("name already exists")
+    
+    brand = Brand(name=data["name"], description=data["description"])
+    brand.save()
+    return jsonify("true")
 
+def is_name_unique(name):
+    existing_name = Brand.objects(name=name).first()
+    return existing_name is None
 
-@app.route("/api/v1/brand/get/<id>", methods=["GET"])
-def get_brand(id):
+@app.route("/api/v1/brand/get/<name>", methods=["GET"])
+def get_brand(name):
     """
-    This API creates a new user
-    :param id:
+    Get brand based on name
+    :param name:
     :return:
     """
-    return "Returned brand. NOT IMPLEMENTED"
-
+    brand = Brand.objects(name=name).first()
+    return jsonify(brand)
 
 @app.route("/api/v1/brand/get", methods=["GET"])
 def get_all_brand():
-    return "Returned all brand. NOT IMPLEMENTED"
+    """
+    Gets all brands
+    :return All brands:
+    """
+    return jsonify(Brand.objects().all())
 
 
 @app.route("/api/v1/brand/update", methods=["PUT"])
-def update_brand(data):
+def update_brand():
     """
     This API creates a new user
-    :param data:
+    :param json data:
     :return:
     """
-    return jsonify("Updated brand. NOT IMPLEMENTED")
+    data = request.get_json()
+    try:
+        brand = Brand.objects.get(id=data["id"])
+        for key, value in data.items():
+            if key in brand:
+                setattr(brand, key, value)
+
+        return jsonify("Updated brand")
+    
+    except Brand.DoesNotExist:
+        return jsonify("Brand does not exist")
+    except Exception as e:
+        return jsonify("Error updating brand")
 
 
-@app.route("/api/v1/brand/delete/<id>", methods=["DELETE"])
-def delete_brand(id):
+@app.route("/api/v1/brand/delete/<name>", methods=["DELETE"])
+def delete_brand(name):
     """
-    This API creates a new user
-    :param id:
+    Deletes brand based on name
+    :param name:
     :return:
     """
-    return jsonify("Deleted brand. NOT IMPLEMENTED")
+    try:
+        brand = Brand.objects.get(name=name)
+        brand.delete()
+        return jsonify("Deleted brand")
+    
+    except Brand.DoesNotExist:
+        return jsonify("User does not exist")
+    except Exception as e:
+        return jsonify("Error deleting user")

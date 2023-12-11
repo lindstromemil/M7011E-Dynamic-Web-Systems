7d9fa90924cbfa8ea81f427623dd8cd276359f3b
fabianwidell <fabian@widell.net>
2023-12-11 12:34:04 +0100

    add handler api for beverage

--- src/internal/models/beverage.py
+++ src/internal/models/beverage.py
@@ -3,15 +3,15 @@ from src.internal.models.brand import Brand
 
 
 class Beverage(Document):
-    name = StringField()
-    description = StringField()
-    image_path = StringField()
-    bitterness = FloatField()
-    fullness = FloatField()
-    sweetness = FloatField()
-    abv = FloatField()
-    beverageType = StringField()
-    country = StringField()
+    name = StringField(required=True)
+    description = StringField(required=True)
+    image_path = StringField(required=True)
+    bitterness = FloatField(required=True)
+    fullness = FloatField(required=True)
+    sweetness = FloatField(required=True)
+    abv = FloatField(required=True)
+    beverageType = StringField(required=True)
+    country = StringField(required=True)
     brand_id = ReferenceField(Brand)
 
     meta = {"collection": "beverages"}

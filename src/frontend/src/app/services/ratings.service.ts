a6082778d65cdcbebac13eafd05f7a0bdd64c7a3
Jesper Svensson <jesperswe@hotmail.com>
2024-01-10 15:23:31 +0100

    implemented admin page

--- src/frontend/src/app/services/ratings.service.ts
+++ src/frontend/src/app/services/ratings.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import {Beverage} from 'src/app/models/beverage.model';
 import { environment } from 'src/environments/environment';
 import {Like} from "../models/like.model";
+import {CookieService} from "ngx-cookie-service";
 
 @Injectable({
   providedIn:'root'
@@ -12,8 +13,15 @@ export class RatingsService {
 
     constructor(
         private http: HttpClient,
+        private cookieService: CookieService
     ) {
 
+    }
+
+    delete_rating(rating_id: string): Observable<string> {
+      const header: {Authorization: string} = { 'Authorization': `Bearer ${this.cookieService.get('token')}` }
+
+      return this.http.delete<string>(environment.baseUrl+"/ratings/"+rating_id, { headers: header });
     }
 
     get_all_rating_likes(rating_id: string): Observable<Like[]> {
